b1b2cba5fe1e53ef63963df790c6542909bf81fb
yu072333 <yzhou9223@gmail.com>
2025-12-16 09:02:33 +0800

    fix: set API_URL to render backend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://smart-fridge-backend-zbkd.onrender.com/api";
 
 
 function App() {
